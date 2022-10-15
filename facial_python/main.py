a2a9a2af2627f96fb4ee50dec4405a560fa50dcf
nmautz <nmautz@zagmail.gonzaga.edu>
2022-10-15 12:36:23 -0700

    config

--- facial_python/main.py
+++ facial_python/main.py
@@ -1,15 +1,20 @@
+import json
 import face_recognition
 import cv2
 import numpy as np
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
+config = None
+try:
+    f = open('../config.json')
+    config = json.load(f)
+    print("Config file loaded for user " + config["user"])
+except FileNotFoundError:
+    print("No Config File Found!")
+    exit(1)
+except Exception as e:
+    print(e)
+    exit(1)
 
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
 
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
