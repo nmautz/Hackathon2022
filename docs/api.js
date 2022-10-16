9511cbbf5b95bb055d597704a7963198dd36b865
nmautz <nmautz@zagmail.gonzaga.edu>
2022-10-15 17:29:40 -0700

    get-videos

--- docs/api.js
+++ docs/api.js
@@ -6,17 +6,15 @@ domain = tmp.toString();
 domain = domain.substring(0, domain.length-1);
 let port = 3000
 
-function get_videos(){
+function get_videos(callback){
 
   let url = `${domain}:${port}/get_videos`;
 
   window.fetch(url, {
     method: 'GET'
-  }).then((data)=>{
-    console.log(data)
+  }).then(data => data.json()).then((data)=>{
+    callback(data)
   })
 
 
 }
-
-get_videos()
