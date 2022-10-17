1a44118dbd2862acc2adcce7a6756fafa1c429a8
nmautz <nmautz@zagmail.gonzaga.edu>
2022-10-17 14:47:42 -0700

    people in video front end

--- docs/video_player.js
+++ docs/video_player.js
@@ -1,5 +1,26 @@
 
 
+function add_person_to_video_people(person, img_src){
+
+  const div = document.createElement("div")
+  const img = document.createElement("img")
+  const p = document.createElement("p")
+
+  div.appendChild(img)
+  div.appendChild(p)
+
+  div.classList.add("person")
+
+  img.src = img_src
+  p.innerHTML = person
+
+  const people_container = document.getElementById("detected-people-container")
+  people_container.appendChild(div)
+
+
+
+}
+
 
 document.addEventListener("DOMContentLoaded", ()=>{
 
